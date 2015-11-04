LINBIT/drbdmanage

commit a8dd00618c483f639f9e86c787e6f15987927a71

Fixed utils add_rc_entry(), server export_conf()

drbdmanage/server.py
@@ -3990,7 +3990,7 @@ class DrbdManageServer(object):
                         add_rc_entry(fn_rc, DM_DEBUG, dm_exc_text(DM_DEBUG))
                 else:
                     add_rc_entry(fn_rc, DM_ENOENT, dm_exc_text(DM_ENOENT),
-                                 RES_NAME, res_name)
+                                 [(RES_NAME, res_name)])
             else:
                 for assg in node.iterate_assignments():
                     if self.export_assignment_conf(assg) != 0:
@@ -5712,8 +5712,8 @@ class DrbdManageServer(object):
     @staticmethod
     def catch_and_append_internal_error(fn_rc, exc):
         msg, args = DrbdManageServer.catch_internal_error(exc)
-        add_rc_entry(fn_rc, DM_DEBUG, msg, *args.items())
-        add_rc_entry(fn_rc, DM_DEBUG, "%s", DM_VERSION + '; ' + DM_GITHASH)
+        add_rc_entry(fn_rc, DM_DEBUG, msg, args)
+        add_rc_entry(fn_rc, DM_DEBUG, "%(versioninfo)s", ["versioninfo", (DM_VERSION + '; ' + DM_GITHASH)])
 
 
 """

drbdmanage/utils.py
@@ -637,7 +637,7 @@ def extend_path(orig_path, ext_path):
     os.environ["PATH"] = sep.join(path)
 
 
-def add_rc_entry(fn_rc, err_code, err_msg, *args):
+def add_rc_entry(fn_rc, err_code, err_msg, args=[]):
     """
     Add a new return code entry to the return codes array
 

commit a25922f9d7d4b1cdef24f088d26b719dd3fbaad2

Fix incorrect use of add_rc_entry() in create_resource()
This bug was introduced with changes committed
in a8dd00618c483f639f9e86c787e6f15987927a71

drbdmanage/server.py
@@ -1626,7 +1626,7 @@ class DrbdManageServer(object):
             resource = self._resources.get(res_name)
             if resource is not None:
                 add_rc_entry(fn_rc, DM_EEXIST, dm_exc_text(DM_EEXIST),
-                             [ RES_NAME, resource.get_name() ])
+                             [ [RES_NAME, resource.get_name()] ])
             else:
                 port = RES_PORT_NR_AUTO
                 secret = generate_secret()