LINBIT/drbdmanage

commit 96f03fb8becd3882a6d0e275eb7f048251fed442

shutdown(): Add capability to shutdown all resources

drbdmanage_client.py
@@ -52,7 +52,7 @@ from drbdmanage.consts import (
     NODE_SITE, NODE_VOL_0, NODE_VOL_1, NODE_PORT, NODE_SECRET,
     DRBDCTRL_LV_NAME_0, DRBDCTRL_LV_NAME_1, DRBDCTRL_DEV_0, DRBDCTRL_DEV_1, NODE_CONTROL_NODE,
     NODE_SATELLITE_NODE, KEY_S_CMD_SHUTDOWN, KEY_ISSATELLITE,
-    KEY_COLORS, KEY_UTF8, NODE_NAME, RES_NAME, SNAPS_NAME,
+    KEY_COLORS, KEY_UTF8, NODE_NAME, RES_NAME, SNAPS_NAME, KEY_SHUTDOWN_RES
 )
 from drbdmanage.utils import SizeCalc
 from drbdmanage.utils import Table
@@ -722,10 +722,14 @@ class DrbdManage(object):
         p_shutdown = subp.add_parser('shutdown',
                                      description='Stops the local drbdmanage server process.')
         p_shutdown.add_argument('-l', '--satellite', action="store_true",
-                                help='If given, also send a shutdown command to connected satellites.')
+                                help='If given, also send a shutdown command to connected satellites.',
+                                default=False)
         p_shutdown.add_argument('-q', '--quiet', action="store_true",
                                 help='Unless this option is used, drbdmanage will issue a safety question '
                                 'that must be answered with yes, otherwise the operation is canceled.')
+        p_shutdown.add_argument('-r', '--resources', action="store_true",
+                                help='Shutdown all drbdmanage-controlled resources too',
+                                default=False)
         p_shutdown.set_defaults(func=self.cmd_shutdown)
 
         # nodes
@@ -2032,8 +2036,10 @@ class DrbdManage(object):
     def cmd_shutdown(self, args):
         quiet = args.quiet
         satellites = args.satellite
+        resources = args.resources
         props = dbus.Dictionary(signature="ss")
         props[KEY_S_CMD_SHUTDOWN] = bool_to_string(satellites)
+        props[KEY_SHUTDOWN_RES] = bool_to_string(resources)
         if not quiet:
             quiet = self.user_confirm(
                 "You are going to shut down the drbdmanaged server "

drbdmanage/consts.py
@@ -64,6 +64,9 @@ KEY_DRBD_KERNEL_GIT_HASH = "drbd_kernel_git_hash"
 KEY_DRBD_UTILS_VERSION   = "drbd_utils_version"
 KEY_DRBD_UTILS_GIT_HASH  = "drbd_utils_git_hash"
 
+# Shut down resources on drbdmanage server shutdown
+KEY_SHUTDOWN_RES = "shutdown-res"
+
 SNAPS_SRC_BLOCKDEV  = "snapshot-source-blockdev"
 
 NODE_NAME_MAXLEN = 128

drbdmanage/drbd/drbdcore.py
@@ -868,6 +868,26 @@ class DrbdManager(object):
         logging.debug("DrbdManager: Exit function initial_up()")
 
 
+    def final_down(self):
+        """
+        Attempts to shut down all resources
+        Used when the drbdmanage server shuts down.
+        """
+        logging.debug("DrbdManager: Enter function final_down()")
+        node = self._server.get_instance_node()
+        if node is not None:
+            for assg in node.iterate_assignments():
+                try:
+                    self._down_resource(assg)
+                except Exception as exc:
+                    logging.debug(
+                        "failed to shut down resource '%s', "
+                        "unhandled exception: %s"
+                        % (assg.get_resource().get_name(), str(exc))
+                    )
+        logging.debug("DrbdManager: Exit function final_down()")
+
+
     def _up_resource(self, assignment):
         """
         Brings up DRBD resources
@@ -919,7 +939,7 @@ class DrbdManager(object):
         # bd_mgr   = self._server.get_bd_mgr()
         resource = assignment.get_resource()
 
-        logging.info("stopping resource '%s'" % (resource.get_name()))
+        logging.info("DrbdManager: Stopping resource '%s'" % (resource.get_name()))
 
         # call drbdadm to bring up the resource
         drbd_proc = self._drbdadm.down(resource.get_name())

drbdmanage/server.py
@@ -53,6 +53,7 @@ from drbdmanage.consts import (
     KEY_SAT_CFG_TCP_KEEPIDLE, KEY_SAT_CFG_TCP_KEEPINTVL, KEY_SAT_CFG_TCP_KEEPCNT,
     DEFAULT_SAT_CFG_TCP_KEEPIDLE, DEFAULT_SAT_CFG_TCP_KEEPINTVL, DEFAULT_SAT_CFG_TCP_KEEPCNT,
     KEY_S_CMD_INIT, KEY_S_ANS_OK, KEY_S_CMD_SHUTDOWN, SAT_CON_SHUTDOWN, SAT_CON_ESTABLISHED, SAT_CON_STARTUP,
+    KEY_SHUTDOWN_RES
 )
 from drbdmanage.utils import NioLineReader
 from drbdmanage.utils import (
@@ -6536,11 +6537,31 @@ class DrbdManageServer(object):
         """
         Stops this drbdmanage server instance
         """
-        logging.info("server shutdown (requested by function call)")
-        if KEY_S_CMD_SHUTDOWN in props and string_to_bool(props[KEY_S_CMD_SHUTDOWN]):
-            for satellite in [s for s, state in self.sat_state_ctrlvol.items() if state == SAT_CON_ESTABLISHED or
-                              state == SAT_CON_SHUTDOWN]:
-                self._proxy.send_cmd(satellite, KEY_S_CMD_SHUTDOWN)
+        logging.info("server shutdown (requested by shutdown API call)")
+        try:
+            shutdown_sat_str = props.get(KEY_S_CMD_SHUTDOWN)
+            if shutdown_sat_str is not None:
+                shutdown_sat = string_to_bool(shutdown_sat_str)
+                if shutdown_sat:
+                    logging.info("shutting down satellites")
+                    for satellite in [sat for sat, state in self.sat_state_ctrlvol.items()
+                                      if state == SAT_CON_ESTABLISHED or state == SAT_CON_SHUTDOWN]:
+                        self._proxy.send_cmd(satellite, KEY_S_CMD_SHUTDOWN)
+        except ValueError:
+            # raised if string_to_bool() is supplied with an invalid value
+            pass
+
+        try:
+            shutdown_res_str = props.get(KEY_SHUTDOWN_RES)
+            if shutdown_res_str is not None:
+                shutdown_res = string_to_bool(shutdown_res_str)
+                if shutdown_res:
+                    logging.info("shutting down drbdmanage-controlled resources")
+                    self._drbd_mgr.final_down()
+        except ValueError:
+            # string_to_bool() again
+            pass
+
         logging.info("shutting down the TCPServer")
         if self._proxy:
             self._proxy.shutdown()
@@ -6550,12 +6571,14 @@ class DrbdManageServer(object):
             self._drbd_mgr.down_drbdctrl()
         except:
             pass
+
         logging.info("shutting down DRBD events processing")
         # Shutdown events processing and the associated child process
         try:
             self.uninit_events()
         except:
             pass
+
         logging.info("server shutdown complete, exiting")
         exit(0)
 

commit 142ad222d1447a3d3b25190a460b6d5f81346de2

Fix server shutdown in cmd_uninit()
This feature is broken in v0.92 due to a change introduced
with commit 96f03fb8becd3882a6d0e275eb7f048251fed442.

drbdmanage_client.py
@@ -2889,7 +2889,8 @@ Confirm:
             if shutdown:
                 try:
                     self.dbus_init()
-                    self._server.shutdown()
+                    props = dbus.Dictionary(signature="ss")
+                    self._server.shutdown(props)
                 except dbus.exceptions.DBusException:
                     # The server does not answer after a shutdown,
                     # or it might not have been running in the first place,