LINBIT/drbdmanage

commit 860426fc1c373ec2d99f2bc553ccb6d88b1e5419

Added StoragePlugin get_trait() function; added Lvm provisioning-type trait
A StoragePlugin's traits are key-value pairs that can be queried by calling
get_trait() with the respective key.

The first trait added to the Lvm* StoragePlugins is the plugin's
provisioning-type.
Key:
  drbdmanage.storage.storagecore.StoragePlugin.KEY_PROV_TYPE
Values:
  drbdmanage.storage.storagecore.StoragePlugin.PROV_TYPE_FAT
  drbdmanage.storage.storagecore.StoragePlugin.PROV_TYPE_THIN

drbdmanage/storage/zvol.py
@@ -88,6 +88,7 @@ class Zvol(StoragePluginCommon, storcore.StoragePlugin):
     _subproc_env = None
 
     def __init__(self, server):
+        self.traits[storcore.StoragePlugin.KEY_PROV_TYPE] = storcore.StoragePlugin.PROV_TYPE_FAT
         self.reconfigure()
 
     def get_default_config(self):

drbdmanage/storage/lvm.py
@@ -22,6 +22,7 @@ import os
 import logging
 import subprocess
 import drbdmanage.storage.lvm_common as lvmcom
+import drbdmanage.storage.storagecore as storcore
 from drbdmanage.storage.storageplugin_common import (
     StoragePluginException, StoragePluginUnmanagedVolumeException)
 
@@ -90,6 +91,7 @@ class Lvm(lvmcom.LvmCommon):
 
     def __init__(self, server):
         super(Lvm, self).__init__()
+        self.traits[storcore.StoragePlugin.KEY_PROV_TYPE] = storcore.StoragePlugin.PROV_TYPE_FAT
         self.reconfigure()
 
     def get_default_config(self):

drbdmanage/storage/lvm_thinlv.py
@@ -22,6 +22,7 @@ import os
 import logging
 import subprocess
 import drbdmanage.storage.lvm_common as lvmcom
+import drbdmanage.storage.storagecore as storcore
 
 import drbdmanage.consts as consts
 import drbdmanage.exceptions as exc
@@ -102,6 +103,7 @@ class LvmThinLv(lvmcom.LvmCommon):
 
     def __init__(self, server):
         super(LvmThinLv, self).__init__()
+        self.traits[storcore.StoragePlugin.KEY_PROV_TYPE] = storcore.StoragePlugin.PROV_TYPE_THIN
         self.reconfigure()
 
     def get_default_config(self):

drbdmanage/storage/lvm_thinpool.py
@@ -113,6 +113,7 @@ class LvmThinPool(lvmcom.LvmCommon):
 
     def __init__(self, server):
         super(LvmThinPool, self).__init__()
+        self.traits[storcore.StoragePlugin.KEY_PROV_TYPE] = storcore.StoragePlugin.PROV_TYPE_THIN
         self.reconfigure()
 
     def get_default_config(self):

drbdmanage/storage/storagecore.py
@@ -382,6 +382,14 @@ class BlockDeviceManager(object):
             self._log_no_plugin()
         return fn_rc
 
+    def get_trait(self, key):
+        """
+        Returns the StoragePlugin's trait value for the specified key, otherwise None
+        """
+        value = None
+        if self._plugin is not None:
+            value = self._plugin.get_trait(key)
+        return value
 
     def _log_not_implemented(self, function_name):
         logging.error(
@@ -481,6 +489,10 @@ class StoragePlugin(object):
     functions it looks for.
     """
     NAME = 'unknown'
+
+    KEY_PROV_TYPE  = "provisioning-type"
+    PROV_TYPE_FAT  = "fat"
+    PROV_TYPE_THIN = "thin"
 
     def __init__(self):
         """
@@ -606,6 +618,12 @@ class StoragePlugin(object):
         @param   node: The node to update
         @type    node: DrbdNode object
         @return: standard return code, pool total size, pool free space
+        """
+        raise NotImplementedError
+
+    def get_trait(self, key):
+        """
+        Returns the trait value selected by the key, otherwise None
         """
         raise NotImplementedError
 

drbdmanage/storage/storageplugin_common.py
@@ -57,8 +57,12 @@ class StoragePluginUnmanagedVolumeException(Exception):
 
 
 class StoragePluginCommon(object):
+
+    # Traits map, str = str key/value pairs
+    traits = None
+
     def __init__(self):
-        pass
+        self.traits = {}
 
     def _deserialize(self, data):
         """
@@ -696,3 +700,6 @@ class StoragePluginCommon(object):
             return self._remove_snapshot(blockdevice)
         except NotImplementedError:
             raise NotImplementedError
+
+    def get_trait(self, key):
+        return self.traits.get(key)

commit 551fdaefe6f6a10a6ccc85b1f518b75ac14870cc

zfs: fix regression introduced in 860426f
In 860426f a "traits" dictionary got introduced. In order to make use of
it, classes have to call init of their parent where this dictionary is
defined. This was not the case for the Zvol plugin, but it tried to set
values in that dictionary. The result is that loading the Zvol plugin
fails. I squashed that half finished change into Robert's commit, it is
me who is to blame.

drbdmanage/storage/zvol.py
@@ -88,6 +88,7 @@ class Zvol(StoragePluginCommon, storcore.StoragePlugin):
     _subproc_env = None
 
     def __init__(self, server):
+        super(Zvol, self).__init__()
         self.traits[storcore.StoragePlugin.KEY_PROV_TYPE] = storcore.StoragePlugin.PROV_TYPE_FAT
         self.reconfigure()
 